e790a2f4e7775eff1c7319e22c696d33f5f60509
Nolan Knight <nolanknight2026@u.northwestern.edu>
2026-03-04 03:10:11 -0600

    Trying to fix thrust phase

--- quadruped_behavior/src/Static_Position.cpp
+++ quadruped_behavior/src/Static_Position.cpp
@@ -309,9 +309,21 @@ public:
                         // Set the FL and BR to the walking phase, and BL and FR to static stance pose, head will be set straight forward for now:
                         walk_joint_state_.clear();
                         process_pose(pose_sequence_[walking_phase_], 0); // FL
-                        process_pose(pose_sequence_[3], 1);              // BL
+                        if(walking_phase_ == PHASE_THRUST) {
+                            // During the thrust phase, static legs will pushback at the hips.
+                            process_pose(pose_sequence_[3], 1);              // BL
+                        }
+                        else {
+                            process_pose(pose_sequence_[0], 1);              // BL
+                        }
                         process_pose(pose_sequence_[walking_phase_], 2); // BR
-                        process_pose(pose_sequence_[3], 3);              // FR
+                        if(walking_phase_ == PHASE_THRUST) {
+                            // During the thrust phase, static legs will pushback at the hips.
+                            process_pose(pose_sequence_[3], 3);              // FR
+                        }
+                        else {
+                            process_pose(pose_sequence_[0], 3);              // FR
+                        }
 
                         walk_joint_state_.push_back(0.0);
                         walk_joint_state_.push_back(0.0);
@@ -319,9 +331,21 @@ public:
                     else { // FR/BL
                         // Set the FL and BR to the walking phase, and BL and FR to static stance pose, head will be set straight forward for now:
                         walk_joint_state_.clear();
-                        process_pose(pose_sequence_[3], 0);                     // FL
+                        if(walking_phase_ == PHASE_THRUST) {
+                            // During the thrust phase, static legs will pushback at the hips.
+                            process_pose(pose_sequence_[3], 0);              // FL
+                        }
+                        else {
+                            process_pose(pose_sequence_[0], 0);              // FL
+                        }
                         process_pose(pose_sequence_[walking_phase_], 1);        // BL
-                        process_pose(pose_sequence_[3], 2);                     // BR
+                        if(walking_phase_ == PHASE_THRUST) {
+                            // During the thrust phase, static legs will pushback at the hips.
+                            process_pose(pose_sequence_[3], 2);              // BR
+                        }
+                        else {
+                            process_pose(pose_sequence_[0], 2);              // BR
+                        }
                         process_pose(pose_sequence_[walking_phase_], 3);        // FR
 
                         walk_joint_state_.push_back(0.0);
