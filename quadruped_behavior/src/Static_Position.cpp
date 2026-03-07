7a6f7baee6d17efc3a02dac228d4f2d7af410ca9
Nolan Knight <nolanknight2026@u.northwestern.edu>
2026-03-06 19:16:46 -0600

    debug

--- quadruped_behavior/src/Static_Position.cpp
+++ quadruped_behavior/src/Static_Position.cpp
@@ -524,8 +524,8 @@ public:
 
             if(front) {
                 // Increase pose and knee and hip slightly
-                pose[0] += 0.05
-                pose[1] += 0.05
+                pose[0] += 0.05;
+                pose[1] += 0.05;
             }
 
             if(flip) {
